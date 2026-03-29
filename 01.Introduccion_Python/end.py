1a26a1f20d4060054ff50afcc56ac316fca14a15
Alison Leon <leonalison877@gmail.com>
2026-03-29 02:19:56 -0500

    Add files via upload

---
+++ 01.Introduccion_Python/end.py
@@ -0,0 +1,9 @@
+#end=
+#deja espacio y sigue en la misma línea
+
+print("Mi nombre es","Pyton.",end=" ")
+print("Monty Python.")
+
+
+print("Mi nombre es ",end="")
+print("Monty Python\n")
---
+++ 01.Introduccion_Python/sep.py
@@ -0,0 +1,7 @@
+#sep=
+
+# sep="-" → coloca un guion entre cada palabra en lugar de espacios
+
+print("Mi","nombre","es","Monty","Pyton.",sep="-")
+print("Mi","nombre","es",sep="_",end="*")
+print("Monty","Python.",sep="*",end="*\n")
